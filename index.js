78248dfd6070d8245ae3c33f91e0de56c647e1ac
Ankit <43137695+andalike@users.noreply.github.com>
2022-05-14 09:47:37 +0530

    Message Change for the custom message.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>RCB Will win IPL</p></body></html>');
+        res.write('<html><body><p>RCB Will win The IPL</p></body></html>');
         res.end();
 
     }
