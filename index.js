e2d9365ff87c64b658407731ee8999c2084bcc9e
Ankit <43137695+andalike@users.noreply.github.com>
2022-05-14 09:46:02 +0530

    The final Truth

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is the Default Page. If You dont specify any end Point/address this default page will be shown.</p></body></html>');
+        res.write('<html><body><p>RCB Will win IPL</p></body></html>');
         res.end();
 
     }
