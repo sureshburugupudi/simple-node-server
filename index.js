fd0a266adb07e301bfdd162da61f2136264c908b
Ankit <43137695+andalike@users.noreply.github.com>
2022-06-21 07:36:01 +0530

    Update to main file.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is the Default Page. If You dont specify any end Point/address this default page will get displayed</p></body></html>');
+        res.write('<html><body><p>This is the Default Page of the Server. If You dont specify any end Point/address this default page will get displayed</p></body></html>');
         res.end();
 
     }
