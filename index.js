4fc2dd90634d75d6ac24cae4d28a57a35d98628b
Ankit <43137695+andalike@users.noreply.github.com>
2022-06-28 07:19:24 +0530

    Message Changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is the Default Page of the Server. If You dont specify any end Point/address this default page will get displayed</p></body></html>');
+        res.write('<html><body><p>This is the Default Page of the Server. If You did not specify any end Point/address this default page will get displayed. Also You should see this message as the proof.</p></body></html>');
         res.end();
 
     }
