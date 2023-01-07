11ef371a67490dda54463b70f84698bcc55968d0
Ankit <43137695+andalike@users.noreply.github.com>
2023-01-07 08:23:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ var server = http.createServer(function (req, res) {
         res.write('<html><body><p>This is Dashboard Page.</p></body></html>');
         res.end();
     }
+    else if (req.url == "/error") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is Error Page.</p></body></html>');
+        res.end();
+    }
     else
         res.end('Invalid Request!');
 });
