77e13a0b718a8c9ae252eb90aea9ef28957ddb25
Ankit <43137695+andalike@users.noreply.github.com>
2023-07-24 07:57:40 +0530

    Modified the homepage content.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var server = http.createServer(function (req, res) {
     }
     else if (req.url == "/home") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is Home Page. Hello All And Good Morning.</p></body></html>');
+        res.write('<html><body><p>This is Home Page. Hello All And Good Morning. This page was added by AWS Code Pipeline. </p></body></html>');
         res.end();
 
     }
