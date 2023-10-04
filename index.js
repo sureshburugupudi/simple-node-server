a5351a7cf8c46af4e13c17e77f03e09a349ed62d
cloudmasters <141547085+sureshburugupudi@users.noreply.github.com>
2023-10-04 22:37:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is the Default Page of thie site of owner.If You have not specified anything or a Address, this page should be visible.This is the update on 5-october-2023</p></body></html>');
+        res.write('<html><body><p>This is the Default Page of thie site of owner of aws.If You have not specified anything or a Address, this page should be visible.This is the update on 5-october-2023</p></body></html>');
         res.end();
 
     }
     else if (req.url == "/home") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is Home Page. Hello All And Good Morning. This page was added by AWS Code Pipeline. </p></body></html>');
+        res.write('<html><body><p>This is Home Page. Hello All And Good Morning. This page was added some content by AWS Code Pipeline. </p></body></html>');
         res.end();
 
     }
