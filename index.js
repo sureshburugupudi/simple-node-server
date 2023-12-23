5c2f329ae277bf730ca4affdc5b8e8c39e67acc5
suresh <141547085+sureshburugupudi@users.noreply.github.com>
2023-12-23 18:02:39 +0530

    Update index.js
    updated the messgae content

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ var server = http.createServer(function (req, res) {
     }
     else if (req.url == "/home") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is Home Page. Hello All And Good Morning. This page was added some content by AWS Code Pipeline. </p></body></html>');
+        res.write('<html><body><p>This is Home Page. Hello All And Good Evening. This page was added some content by AWS Code Pipeline. </p></body></html>');
         res.end();
 
     }
     else if (req.url == "/dashboard") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is Dashboard Page. Hello All And Good Morning. Iam adding some content in this website.</p></body></html>');
+        res.write('<html><body><p>This is Dashboard Page. Hello All And Good Evening. Iam adding some content in this website.</p></body></html>');
         res.end();
     }
     else if (req.url == "/error") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is Error Page. Hello All And Good Morning.</p></body></html>');
+        res.write('<html><body><p>This is Error Page. Hello All And Good Evening.</p></body></html>');
         res.end();
     }
     else
